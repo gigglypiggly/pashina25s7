f4c04a626a1dfc11b7a2cd95550f9fa75a3ef70f
gigglypiggly <altai.pashin@dcmail.ca>
2025-07-09 15:52:18 -0400

    doing examples

---
+++ week9/js/w9ex1.js
@@ -0,0 +1,8 @@
+/*
+ Name: Altai
+ Student number: 101007054
+ Date: 9/7/2025
+ Description: 
+*/
+
+console.log("My name is Altai");
---
+++ week9/js/w9ex2.js
@@ -0,0 +1,11 @@
+/*
+ Name: Altai
+ Student number: 101007054
+ Date: 9/7/2025
+ Description: 
+*/
+
+function showGreetingMessage() {
+    let name = window.prompt("What is your name?");
+    window.alert("Hello " + name + "!");
+}
